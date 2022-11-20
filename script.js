e7ca87bfd7b670dd8e5f9a023dafc6b48c0683c9
WinserEX <winser500@gmail.com>
2022-11-19 22:43:04 -0400

    1st commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let zoo = {
+    cat: 
+}
+let animals = ["Cat", "Dog", "Giraffe", "Croc"]
+let table = document.querySelector("#table")
+
+for (animal of animals) {
+    let a = document.createElement("tr");
+    let b = document.createElement("td");
+    table.append(a);
+    table.append(b);
+    b.append(animal);
+}
