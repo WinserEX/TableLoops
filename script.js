c400e18c93671281bf8a2e466cc5ac3200450e00
WinserEX <winser500@gmail.com>
2022-11-20 12:59:19 -0400

    latest changes

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ for (animal of animals) {
     table.append(b);
     b.append(animal);
 }
+
+
+
+let table2 = document.querySelector("#table2")
+
+let zoo = [
+    {name: "Cat", id: 1},
+    {name: "Dog", id: 2},
+    {name: "Parrot", id: 3},
+    {name: "Monkey", id: 4},
+    {name: "Lion", id: 5}
+]
+//Tests
+console.log(zoo[0].name, zoo[0].id);
+
+for (beast of zoo) {
+    let a = document.createElement("tr");
+    let b = document.createElement("td");
+    let c = document.createElement("td");
+    table2.append(a);
+    a.append(b,c);
+    b.append(beast.id)
+    c.append(beast.name)  
+}
